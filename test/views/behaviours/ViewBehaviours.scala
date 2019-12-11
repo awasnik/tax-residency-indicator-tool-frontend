d1b0ced39579fb0e92fb8e0c52e20fa1a696731b
“awasnik” <ankit.wasnik@accenture.com>
2019-12-11 12:42:48 +0000

    TRIT-130: Removing language toggle

--- test/views/behaviours/ViewBehaviours.scala
+++ test/views/behaviours/ViewBehaviours.scala
@@ -54,12 +54,6 @@ trait ViewBehaviours extends ViewSpecBase {
           val doc = asDocument(view)
           for (key <- expectedGuidanceKeys) assertContainsText(doc, messages(s"$messageKeyPrefix.$key"))
         }
-
-        "display language toggles" in {
-
-          val doc = asDocument(view)
-          assertRenderedByCssSelector(doc ,".lang")
-        }
       }
     }
   }
